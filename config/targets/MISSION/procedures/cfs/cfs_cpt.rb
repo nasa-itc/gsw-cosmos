13dc0ce538b56e11ce3a645ad8b48baea935116c
dacarter22 <davidacarter22@gmail.com>
2025-04-24 17:20:04 +0000

    [nasa/nos3#589] removed cf_cmd prefix because it broke noop tests

--- config/targets/MISSION/procedures/cfs/cfs_cpt.rb
+++ config/targets/MISSION/procedures/cfs/cfs_cpt.rb
@@ -1,12 +1,3 @@
-require "sample_lib.rb"
-require "generic_adcs_lib.rb"
-require "generic_css_lib.rb"
-require "generic_eps_lib.rb"
-require "generic_fss_lib.rb"
-require "generic_imu_lib.rb"
-require "generic_mag_lib.rb"
-require "generic_torquer_lib.rb"
-require "gps_lib.rb"
 require "cfs_lib.rb"
 
 # Setup radio and enable instruments
@@ -54,8 +45,8 @@ cmd("CFS_RADIO MM_NOOP")
 cmd("CFS_RADIO SCH_NOOP")
 sleep(CFS_CMD_SLEEP)
 
-#CFDP Large C1
-#Confirm radio operational
+CFDP Large C1
+Confirm radio operational
 enable_TO_and_verify()
 # Uplink
 cmd("CFS_RADIO FM_DELETE with FILENAME '/data/tmp1_c1.so'")
@@ -94,36 +85,13 @@ wait_check("CFDP CFDP_ENGINE_HK ENG_INPROGRESSTRANS == 1", 10)
 wait_check("CFDP CFDP_ENGINE_HK ENG_INPROGRESSTRANS == 0", 20)
 wait_check("CFDP CFDP_ENGINE_HK ENG_TOTALSUCCESSTRANS > #{initial_success_count}", 20)
 
-#Components 
-# sample
-sample_cmd("SAMPLE SAMPLE_NOOP_CC")
-
-# css
-generic_css_cmd("GENERIC_CSS GENERIC_CSS_NOOP_CC")
-
-# fss
-fss_cmd("GENERIC_FSS GENERIC_FSS_NOOP_CC")
-
-# imu
-cf_cmd("GENERIC_IMU GENERIC_IMU_NOOP_CC")
-
-# mag
-generic_mag_cmd("GENERIC_MAG GENERIC_MAG_NOOP_CC")
-
-# torquer
-generic_torquer_cmd("GENERIC_TORQUER GENERIC_TORQUER_NOOP_CC")
-
-# gps
-gps_cmd("NOVATEL_OEM615 NOVATEL_OEM615_NOOP_CC")
-
-
 #CFS System Tests
 cmd("CFS CFE_ES_NOOP")
 cmd("CFS CFE_TBL_NOOP")
 cmd("CFS CFE_TIME_NOOP")
 cmd("CFS CF_NOOP")
 cmd("CFS CI_NOOP_CC")
-cmd("CFS CS_NOOP_CC")
+cmd("CFS CS_NOOP")
 cmd("CFS FM_NOOP")
 cmd("CFS HK_NOOP")
 cmd("CFS HS_NOOP")
