194e59aebba0878a710a6a746358b2b624218ca8
Mark Suder <mark.d.suder@nasa.gov>
2025-04-24 11:44:32 -0400

    https://github.com/nasa/nos3/issues/330 - Add nominal_ops ruby scriipt.

---
+++ config/targets/MISSION/procedures/nominal_ops.rb
@@ -0,0 +1,27 @@
+require 'mission_lib'
+
+enable_TO_and_verify()
+sleep(1)
+
+battery_threshold = 15.00 # volts
+battery_bus_voltage = tlm("GENERIC_EPS_RADIO GENERIC_EPS_HK_TLM BATT_VOLTAGE")
+
+if battery_bus_voltage < battery_threshold
+    should_cancel = message_box("Battery bus voltage below #{battery_threshold}V threshold! Abort pass?", 'Yes', 'No', false)
+    case should_cancel
+    when 'Yes'
+        cmd("CFS_RADIO TO_PAUSE_OUTPUT")
+    when 'No'
+        wait(3) # Prompt twice to make sure user is REALLY sure
+        really_should_cancel = message_box("Battery bus voltage below #{battery_threshold}V threshold! Abort pass?", 'Yes', 'No', false)
+        case really_should_cancel
+        when 'Yes'
+            cmd("CFS_RADIO TO_PAUSE_OUTPUT")
+        end 
+    end
+end
+
+puts "STF nominal pass script complete!  Perform any necessary commanding now."
+raise "Pausing script - press go to resume and conclude the pass."
+
+cmd("CFS_RADIO TO_DISABLE_OUTPUT")
